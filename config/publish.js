7c48d04c995dafd2418dac644a6a3bba540ab131
宿愿Cc <375642570@qq.com>
2022-03-14 10:33:47 +0800

    feat: 修改 发布命令

--- config/publish.js
+++ config/publish.js
@@ -2,7 +2,11 @@ const { publishCommand } = require('./constants.js');
 const { exec } = require('./utils.js');
 
 async function publish() {
-  await exec(publishCommand);
-  console.log('项目更新成功');
+  for (const item of publishCommand) {
+    await exec(item, { cwd: '/www/wwwroot/pofile/blog', stdio: 'inherit' });
+    console.log('成功: ', item);
+  }
+
+  console.log('项目更新成功~');
 }
 publish();
