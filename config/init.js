8a79a3bed0883e8a67dd9828079cffde10305892
茂茂 <375642570@qq.com>
2022-04-22 11:10:36 +0800

    chore: 新增 init:dev 命令, 本地端跳过环境变量,七牛云账号等检测

--- config/init.js
+++ config/init.js
@@ -5,6 +5,9 @@ const dotenv = require('dotenv');
 const { exec } = require('./utils.js');
 const { getAccountCommand, toPath, fromPath } = require('./constants.js');
 
+// dev 模式跳过环境变量检测，install，设置七牛云账号，只copy自定义样式
+const initType = process.argv[2].split('=')[1];
+
 /**
  * 初始化配置
  * 1. 配置七牛云账户
@@ -12,18 +15,23 @@ const { getAccountCommand, toPath, fromPath } = require('./constants.js');
  * 3. 修改部分模块源码
  */
 async function init() {
-  if (!(await preParse())) return;
-  if (!process.env.ACCESS_KEY || !process.env.SECRET_KEY) return;
+  if (initType !== 'dev') {
+    if (!(await preParse())) {
+      return console.log(
+        'env 配置文件未填写，请在 https://portal.qiniu.com/user/key 将 Access/Secret Key 复制到项目 .env 文件下后再进行重试'
+      );
+    }
 
-  await exec('pnpm install'); // 1. 安装依赖
+    await exec('pnpm install'); // 1. 安装依赖
 
-  await exec(getAccountCommand()); // 2. 设置 七牛云账号
+    await exec(getAccountCommand()); // 2. 设置 七牛云账号
 
-  await exec('qshell account'); // 查看 七牛云账号
+    await exec('qshell account'); // 查看 七牛云账号
+  }
 
   // 3. 设置自定义样式
   try {
-    const res = Promise.all([
+    const res = await Promise.all([
       copy(`${fromPath}/index.ejs`, `${toPath}/layout/index.ejs`),
       copy(`${fromPath}/custom.css`, `${toPath}/source/css/custom.css`),
     ]);
@@ -35,16 +43,18 @@ async function init() {
 
 async function preParse() {
   const dotenvPath = path.resolve(__dirname, '../.env');
+
   if (pathExists(dotenvPath)) {
     dotenv.config({ path: dotenvPath });
-    return true;
   } else {
-    console.log(
-      'env 配置文件未填写，请在 https://portal.qiniu.com/user/key 将 Access/Secret Key 复制到项目 .env 文件下后再进行重试'
-    );
     await copy(`${fromPath}/.env`, `../.env`);
-    return false;
   }
+
+  function checkEnv() {
+    return process.env.ACCESS_KEY && process.env.SECRET_KEY;
+  }
+
+  return checkEnv();
 }
 
 function copy(from, to) {
